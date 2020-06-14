d59a94200a8557fdc63acbe05d83bd74bd3271e5
zhang33 <909807460@qq.com>
2020-06-14 23:17:03 +0800

    ddd

--- src/main/java/com/jiang/CmsApplication.java
+++ src/main/java/com/jiang/CmsApplication.java
@@ -9,6 +9,8 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 public class CmsApplication {
 
     public static void main(String[] args) {
+        System.out.println("ccc");
+
         SpringApplication.run(CmsApplication.class, args);
     }
 
